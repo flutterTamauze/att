737b2d90786fa5442d9d2742a971a7ee4bd7b3a0
[your <[Flutter@tamauzeds.com]>
2021-11-30 17:22:06 +0200

    external missions added

--- lib/constants.dart
+++ lib/constants.dart
@@ -30,9 +30,9 @@ List<String> weekDays = [
   "الخميس",
   "الجمعة"
 ];
-DateTime kAndroidReleaseDate = DateTime(DateTime.now().year, 11, 29);
+DateTime kAndroidReleaseDate = DateTime(DateTime.now().year, 11, 30);
 DateTime _currentBackPressTime;
-DateTime kiosReleaseDate = DateTime(DateTime.now().year, 11, 22);
+DateTime kiosReleaseDate = DateTime(DateTime.now().year, 11, 30);
 //ERRORS
 const USER_INVALID_RESPONSE = 100;
 const NO_INTERNET = 101;
